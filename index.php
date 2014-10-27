3d8230ef4971bf8921d963611726cca2b4ed04b1
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-27 20:47:14 +0800

    initial index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	  <link rel="stylesheet" type="text/css" href="vendors/stylesheets/jquery.fullPage.css" />
+  </head>
+  <body>
+    <?php include 'test.php' ;?>
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="vendors/javascripts/jquery.fullPage.js"></script>
+  </body>
+</html>
+
+	
