4667e413550985168897b1eb6dd2b8834e6b9364
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-03 12:43:22 +0800

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,38 +5,39 @@ var sass = require('gulp-ruby-sass');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var browserSync = require('browser-sync');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', ['html', 'css', 'js'], function () {
 });
 
 gulp.task('html', function() {
   var opts = {comments:true,spare:true};
-  gulp.src('*.html')
+  gulp.src('app/*.html')
   .pipe(minifyHTML(opts))
   .pipe(gulp.dest('dist/html'))
 });
 
 gulp.task('css', function() {
-  return gulp.src('assets/stylesheets/*.css')
+  return gulp.src('app/assets/css/*.css')
     .pipe(csso())
     .pipe(gulp.dest('dist/css'));
  });
 
  gulp.task('js', function() {
-     gulp.src('assets/javascripts/*.js')
+     gulp.src('app/assets/js/*.js')
       .pipe(uglify())
       .pipe(gulp.dest('dist/js'))
  });
 
  gulp.task('scss', function () {
-  return gulp.src('assets/scss/*.scss')
+  return gulp.src('app/assets/scss/*.scss')
     .pipe(sass({sourcemap: true, sourcemapPath: '../scss'}))
     .on('error', function (err) { console.log(err.message); })
     .pipe(gulp.dest('dist/css/'));
  });
 
 gulp.task('images', function () {
-    return gulp.src('assets/images/*')
+    return gulp.src('app/assets/img/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
