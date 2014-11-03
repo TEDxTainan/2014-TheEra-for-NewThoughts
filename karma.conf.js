b2188d1bb97f51e70eca227bb5b4b3514a90299c
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-03 12:35:48 +0800

    update package.json add testing

---
+++ karma.conf.js
@@ -0,0 +1,30 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : './',
+
+    files : [
+      'app/assets/js/*.js',
+      'app/vendors/js/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
