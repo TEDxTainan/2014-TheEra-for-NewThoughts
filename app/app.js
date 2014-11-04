8a4158736a5218641f63ed28e4194df97fba0a65
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-04 14:58:40 +0800

    add anchor and toogle image

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 (function() {
   var app = angular.module('event', [ ]);
   
-  app.controller('MenuController', function(){
+  app.controller('MenuController', ['$location', '$anchorScroll', function($location, $anchorScroll){
     this.images = [
           {
             svg: 'assets/img/unfold.svg', 
@@ -16,16 +16,31 @@
             png: 'assets/img/select.png'
           }];
     
-    this.image = this.images[0]
-    this.setImg = function(i){
-      this.image = this.images[i];
+    this.index = 0;
+    this.anchor = 0;
+    this.image = this.images[0];
+
+    this.toggleImg = function(){
+      this.index = this.index == 1 ? 0 : 1;
+      this.image = this.images[this.index];
     };
 
-    this.isSelect = function(i){
-      return this.image === this.images[i];
+    this.isSelect = function(){
+      return (this.image === this.images[1]) || (this.image === this.images[2]);
+    };
+    
+    this.gotoAnchor = function(x){
+      this.anchor = x;
+      this.image = this.images[2];
+      $.fn.fullpage.moveTo(x);
     };
 
-  });
+    this.isAnchor = function(x) {
+      console.log(x);
+      console.log(this.anchor);
+      return this.anchor === x;
+    }
+  }]);
 
   app.controller('ShareController', function(){
     this.images = [
@@ -46,10 +61,16 @@
     this.isSelect = function(i){
       return this.image === this.images[i];
     };
+
   });
+
 
   app.controller('SpeakerController', function(){
   
   });
+
+
+
+
 
 }) ();
--- app/assets/js/new.thoughts.js
+++ app/assets/js/new.thoughts.js
@@ -1,32 +1,12 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    'verticalCentered': false,
-  'css3': true,
-	 menu: '#menu',
-	 anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage'],
-  'sectionsColor': ['#fff', '#fff', '#fff', '#fff'],
-  'navigation': true,
-  'navigationPosition': 'right',
-  'navigationTooltips': ['fullPage.js', 'Powerful', 'Amazing', 'Simple'],
+   'css3': true,
+   'sectionsColor': ['#fff', '#fff', '#fff', '#fff'],
+		anchors: ['home', 'section1', 'section2', 'section3', 'join'],
+		menu: '#menu',
+		'navigation': true,
+		'navigationPosition': 'right'
 
-  'afterLoad': function(anchorLink, index){
-    if(index == 2){
-      $('#iphone3, #iphone2, #iphone4').addClass('active');
-    }
-  },
-
-  'onLeave': function(index, nextIndex, direction){
-    if (index == 3 && direction == 'down'){
-      $('.section').eq(index -1).removeClass('moveDown').addClass('moveUp');
-    }
-    else if(index == 3 && direction == 'up'){
-      $('.section').eq(index -1).removeClass('moveUp').addClass('moveDown');
-    }
-
-    $('#staticImg').toggleClass('active', (index == 2 && direction == 'down' ) || (index == 4 && direction == 'up'));
-    $('#staticImg').toggleClass('moveDown', nextIndex == 4);
-    $('#staticImg').toggleClass('moveUp', index == 4 && direction == 'up');
-  }
   });
 });
 
