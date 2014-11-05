e8aab8e52a34eb6f981a5fe1bf103db208e47c98
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-05 09:07:48 +0800

    move base and device response

--- app/assets/js/new.thoughts.js
+++ app/assets/js/new.thoughts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
    'css3': true,
    'sectionsColor': ['#fff', '#fff', '#fff', '#fff'],
-		anchors: ['home', 'section1', 'section2', 'section3', 'participate'],
+		anchors: ['home', 'brief', 'section1', 'section2', 'section3', 'participate'],
 		menu: '#menu',
 		'navigation': false,
 		'navigationPosition': 'right'
