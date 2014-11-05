5b085706a83e1517bc6df3856fdff1c862f75357
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-05 17:31:14 +0800

    update app.js change scope

--- app.js
+++ app.js
@@ -1,56 +1,60 @@
-(function() {
-  var app = angular.module('event', ['ngRoute','djds4rce.angular-socialshare']);
+var app = angular.module('event', ['ngRoute', 'djds4rce.angular-socialshare']);
 
-  app.config(function($locationProvider){
+  /*app.config(function($locationProvider, $rootScopeProvider){
         $locationProvider.html5Mode(true).hashPrefix('!');
-  });
+        $rootScopeProvider.digestTtl(20);
+  });*/
 
-  app.run(function($FB){
-    $FB.init('164546407087109');
-  });
+ app.run(function($FB){
+  $FB.init('164546407087109');
+ });
 
+ app.controller('MenuController', ['$scope', '$location', '$anchorScroll', 
+     function($scope, $location, $anchorScroll){
+  $scope.images = [
+        {
+          svg: '//www.tedxtainan.com/app/assets/img/unfold.svg', 
+          png: '//www.tedxtainan.com/app/assets/img/unfold.png'
+        },
+        {
+          svg: '//www.tedxtainan.com/app/assets/img/unselect.svg', 
+          png: '//www.tedxtainan.com/app/assets/img/unselect.png'
+        },
+        {
+          svg: '//www.tedxtainan.com/app/assets/img/select.svg', 
+          png: '//www.tedxtainan.com/app/assets/img/select.png'
+        }];
   
-  app.controller('MenuController', ['$location', '$anchorScroll', function($location, $anchorScroll){
-    this.images = [
-          {
-            svg: '//www.tedxtainan.com/app/assets/img/unfold.svg', 
-            png: '//www.tedxtainan.com/app/assets/img/unfold.png'
-          },
-          {
-            svg: '//www.tedxtainan.com/app/assets/img/unselect.svg', 
-            png: '//www.tedxtainan.com/app/assets/img/unselect.png'
-          },
-          {
-            svg: '//www.tedxtainan.com/app/assets/img/select.svg', 
-            png: '//www.tedxtainan.com/app/assets/img/select.png'
-          }];
-    
-    this.index = 0;
-    this.anchor = 0;
-    this.image = this.images[0];
+  this.index = 0;
+  this.anchor = 0;
+  var change = false;
+  $scope.image = $scope.images[0];
 
-    this.toggleImg = function(){
-      this.index = this.index == 1 ? 0 : 1;
-      this.image = this.images[this.index];
-    };
+  $scope.toggleImg = function(){
+    this.index = this.index == 1 ? 0 : 1;
+    $scope.image = $scope.images[this.index];
+    console.log(this.index);
+  };
+  $scope.isSelect = function(){
+    console.log($scope);
+    change = ($scope.image === $scope.images[1]) || ($scope.image === $scope.images[2]); 
+    return change;
+  };
+  
+  $scope.gotoAnchor = function(x){
+    this.anchor = x;
+    $scope.image = $scope.images[2];
+    $.fn.fullpage.moveTo(x);
+  };
 
-    this.isSelect = function(){
-      return (this.image === this.images[1]) || (this.image === this.images[2]);
-    };
-    
-    this.gotoAnchor = function(x){
-      this.anchor = x;
-      this.image = this.images[2];
-      $.fn.fullpage.moveTo(x);
-    };
+  $scope.isAnchor = function(x) {
+    return this.anchor === x;
+  }
+}]);
 
-    this.isAnchor = function(x) {
-      return this.anchor === x;
-    }
-  }]);
-
-  app.controller('ShareController', function(){
-    this.images = [
+app.controller('ShareController', ['$scope', 
+  function($scope){
+    $scope.images = [
               {
                 svg: '//www.tedxtainan.com/app/assets/img/unclickbutton.svg', 
                 png: '//www.tedxtainan.com/app/assets/img/unclickbutton.png'
@@ -59,20 +63,20 @@
                 svg: '//www.tedxtainan.com/app/assets/img/clickbutton.svg', 
                 png: '//www.tedxtainan.com/app/assets/img/clickbutton.png'
               }];
-    this.image = this.images[0]
+    $scope.image = $scope.images[0]
 
     this.isOpen = false;
 
-    this.toggle = function(){
+    $scope.toggle = function(){
       this.isOpen = this.isOpen? false : true;
     }
 
-  });
+  }]);
 
 
-  app.controller('ThemeController', function(){
+  app.controller('ThemeController', ['$scope', function($scope){
       
-    this.speakers = [{
+    $scope.speakers = [{
                       image: {
                           full: '',
                           thumb: 'assets/img/unknown_speaker.svg'
@@ -116,8 +120,4 @@
                     }
                   ];
 
-
-  });
-
-
-}) ();
+  }]);
