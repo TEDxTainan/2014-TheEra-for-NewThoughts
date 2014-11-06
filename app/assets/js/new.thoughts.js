ef63835f568b4f6972cb7116a8ddd91660186022
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-06 21:04:47 +0800

    scroll active menu

--- app/assets/js/new.thoughts.js
+++ app/assets/js/new.thoughts.js
@@ -5,7 +5,15 @@ $(document).ready(function() {
 		anchors: ['home', 'brief', 'section1', 'section2', 'section3', 'participate'],
 		menu: '#menu',
 		'navigation': false,
-		'navigationPosition': 'right'
+		'navigationPosition': 'right',
+      afterLoad: function(anchor, index){
+        var activeItem;
+        var e = "#menu li" + ":nth-child(" + (index-1).toString() + ")";
+        activeItem = $(e);
+        activeItem
+            .addClass('active')
+            .siblings().removeClass('active');
+    }
 
   });
 });
